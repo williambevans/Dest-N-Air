e446bdf097ec2e47fe4dd6352412d0ef02f02f46
WHBJ <bevansrealtors@gmail.com>
2019-01-28 17:55:26 -0600

    revert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 
 $(document).ready(function(){
-    var queryURL = "https://api.airvisual.com/v2/city?city=LosAngeles&state=California&country=USA&key=BtNrfeJaZn6KRohbs";
+    var queryURL = "http://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key=BtNrfeJaZn6KRohbs";
       
     $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
         console.log(response);
     });
 });
