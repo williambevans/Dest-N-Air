fce67cee7f0c8f678f1a0668511df9631276b1d7
Eryn <johnmary1010@gmail.com>
2019-01-29 14:46:14 -0600

    updates css grid -weather and console log input to ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,19 @@
 
 $(document).ready(function(){
-    var queryURL = "http://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key=BtNrfeJaZn6KRohbs";
+
+    $('.btn-primary').on("click", function(event){
+        event.preventDefault();
+        var cityElement = $('#city').val();
+        var stateElement = $('#state').val();
+        var countryElement = $('#country').val();
+
+        var queryURL ='http://api.airvisual.com/v2/city?city='+cityElement+'&state='+stateElement+'&country='+countryElement+'&key=BtNrfeJaZn6KRohbs';
       
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-
         console.log(response);
+    });
     });
 });
