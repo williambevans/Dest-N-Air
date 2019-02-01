4275cb82b374036d3f2e175348ece476afc60b9d
Eryn <johnmary1010@gmail.com>
2019-02-01 16:29:26 -0600

    added drag to the cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,4 +101,41 @@ $(document).ready(function () {
         }
 
     });
+
+     //adds drag class and removes drag class
+     $(function(){
+        $('.card').hover(function(){
+            $(this).addClass('dragDiv');
+            console.log(this);
+            $('.dragDiv').mousedown(handle_mousedown);
+        },
+        function() {
+            $(this).removeClass('dragDiv');
+            console.log(this);
+        }
+        
+        );
+    });
+    //drag function
+    function handle_mousedown(e){
+        window.my_dragging = {};
+        my_dragging.pageX0 = e.pageX;
+        my_dragging.pageY0 = e.pageY;
+        my_dragging.elem = this;
+        my_dragging.offset0 = $(this).offset();
+        function handle_dragging(e){
+            var left = my_dragging.offset0.left + (e.pageX - my_dragging.pageX0);
+            var top = my_dragging.offset0.top + (e.pageY - my_dragging.pageY0);
+            $(my_dragging.elem)
+            .offset({top: top, left: left});
+        }
+        function handle_mouseup(e){
+            $('body')
+            .off('mousemove', handle_dragging)
+            .off('mouseup', handle_mouseup);
+        }
+        $('body')
+        .on('mouseup', handle_mouseup)
+        .on('mousemove', handle_dragging);
+    }
 });
