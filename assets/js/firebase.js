4277c138b881b7aa0c8f1cbfb05c473a075962b5
Eryn <johnmary1010@gmail.com>
2019-02-01 18:28:15 -0600

    updated login stuff

---
+++ assets/js/firebase.js
@@ -0,0 +1,16 @@
+var app_firebase = {};
+(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAfj2WK0xN8tscrKCME6Qzkgj2E1xRo8ng",
+    authDomain: "destin-air.firebaseapp.com",
+    databaseURL: "https://destin-air.firebaseio.com",
+    projectId: "destin-air",
+    storageBucket: "destin-air.appspot.com",
+    messagingSenderId: "753142024366"
+};
+firebase.initializeApp(config);
+app_firebase = firebase;
+
+})()
