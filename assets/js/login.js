af310094d1341cc80c8ae28f20a2f63728432966
WHBJ <bevansrealtors@gmail.com>
2019-02-03 17:03:55 -0600

    log in page update

--- assets/js/login.js
+++ assets/js/login.js
@@ -22,16 +22,13 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
       signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID
       ],
-      // Terms of service url.
-      tosUrl: 'terms.html',
-      // Privacy policy url.
-      privacyPolicyUrl: 'terms.html'
+      
     };
 
     // The start method will wait until the DOM is loaded.
